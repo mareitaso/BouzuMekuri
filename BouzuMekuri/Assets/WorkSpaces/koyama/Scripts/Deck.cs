264b5ccf1ff0ee4bb35d33e6206c771c8debe7b0
koyama <>
2020-05-23 11:46:00 +0900

    TestScript

---
+++ BouzuMekuri/Assets/WorkSpaces/koyama/Scripts/Deck.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    [SerializeField]
+    List<int> cards;//リストの宣言
+
+    public IEnumerable<int> GetCards()
+    {
+        foreach (int i in cards)//cardsの要素
+        {
+            yield return i;//要素を戻り値に返す
+        }
+    }
+
+    public void Shuffle()
+    {
+        if(cards == null)
+        {
+            cards = new List<int>();//初期化
+        }
+        else
+        {
+            cards.Clear();//cardsを空にする
+        }
+
+        for (int i = 0; i < 100; i++)
+        {
+            cards.Add(i);
+        }
+
+        int n = cards.Count;//nの初期値はカードの枚数
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);//
+            int temp = cards[k];//k番目のカードをtempに追加
+            cards[k] = cards[n];
+            cards[n] = temp;
+        }
+    }
+    private void Start()
+    {
+        Shuffle();//実行
+    }
+    public void Draw()
+    {
+        
+        int cou = cards.Count;
+        cards.RemoveAll(cards=>(int)cards == 50);//リスト内から50が削除される
+    }
+}
---
+++ BouzuMekuri/Assets/WorkSpaces/koyama/Scripts/HandCount.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCount : MonoBehaviour
+{
+    public static HandCount Instance;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public int handCount;//手札の枚数
+    public int DiscardCount;//捨て札の枚数
+}
