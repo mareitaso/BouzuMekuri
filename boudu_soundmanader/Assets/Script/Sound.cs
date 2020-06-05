afd3d9e5352f265e6fbfd647a49471901d474ba6
kabuto <0cp1v1527251r6v@ezweb.ne.jp>
2020-06-05 18:21:19 +0900

    BGM・SE同時に鳴る SEはクリックしたときのみ

---
+++ boudu_soundmanader/Assets/Script/Sound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+
+    public AudioClip SE1;
+
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();//音を代入
+    }
+
+    private void Update()
+    {
+        //左クリックを受け付ける
+        if (Input.GetMouseButtonDown(0))
+        {
+            audioSource.PlayOneShot(SE1);
+            Debug.Log("鳴ったー");
+        }
+    }
+
+}
