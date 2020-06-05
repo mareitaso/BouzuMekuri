e7b5c04fc4062654669e4bf14e7a7be86165b48f
kabuto <0cp1v1527251r6v@ezweb.ne.jp>
2020-06-05 14:09:21 +0900

    BGMは鳴る

---
+++ boudu_soundmanader/Assets/Script/Botann.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botann : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //左クリックを受け付ける
+        if (Input.GetMouseButtonDown(0))
+            Debug.Log("Pressed primary button.");
+    }
+}
---
+++ boudu_soundmanader/Assets/Script/ClickCard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickCard : MonoBehaviour
+{
+
+    private AudioSource sound01;
+    private AudioSource sound02;
+
+    void Start()
+    {
+        AudioSource[] audioSources = GetComponents<AudioSource>();//音を代入
+        sound01 = audioSources[0];
+        //sound02 = audioSources[1];
+    }
+
+    public void OnClick()
+    {
+        sound01.PlayOneShot(sound01.clip);// クリックしたら音が鳴る
+    }
+    //public void OnClick2()
+    //{
+    //    sound02.PlayOneShot(sound02.clip);
+    //}
+}
