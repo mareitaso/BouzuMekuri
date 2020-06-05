329c8b047e679c442b3ce521fbba25a98a7a02a5
TanakaAmi <amiamico0129@gmail.com>
2020-06-05 18:52:55 +0900

    シーンマネージャー製作途中段階

---
+++ BouzuMekuri/Assets/WorkSpaces/Tanaka/Scripts/ManagerScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerScene : MonoBehaviour
+{
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void loadManagerScene()
+    {
+        // Manager という名前の GameObject を作成し、Manager というクラスを Add する
+        string managerSceneName = "ManagerScene";
+
+        if (!SceneManager.GetSceneByName(managerSceneName).IsValid())
+        {
+            SceneManager.LoadScene(managerSceneName, LoadSceneMode.Additive);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
