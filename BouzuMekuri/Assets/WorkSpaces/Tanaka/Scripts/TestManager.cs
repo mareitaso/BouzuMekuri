506d1438f3ab6e8ea17c1ed995b80b3598e2cf68
TanakaAmi <amiamico0129@gmail.com>
2020-06-12 18:13:15 +0900

    シーンの取得

---
+++ BouzuMekuri/Assets/WorkSpaces/Tanaka/Scripts/TestManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestManager : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private IEnumerator Start()
+    {
+        Scene sceneA = SceneManager.GetSceneByName("TitleTestScene");
+        Debug.LogFormat("TitleTestScene = {0}", sceneA.IsValid());
+        Scene sceneB = SceneManager.GetSceneByName("ResultScene");
+        Debug.LogFormat("ResultScene = {0}", sceneB.IsValid());
+
+        yield return null;
+    }
+
+}
