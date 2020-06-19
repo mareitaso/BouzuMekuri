dc9d5bbdb4558828ade24b8d41101b3f92d103dc
kabuto <0cp1v1527251r6v@ezweb.ne.jp>
2020-06-19 09:37:40 +0900

    フェードアウト・停止可能

--- boudu_soundmanader/Assets/Script/Sound.cs
+++ boudu_soundmanader/Assets/Script/Sound.cs
@@ -7,21 +7,24 @@ public class Sound : MonoBehaviour
 
     public AudioClip SE1;
 
-    AudioSource audioSource;
+    public AudioSource audioSource;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();//音を代入
     }
 
-    private void Update()
+    void Update()
     {
+       
         //左クリックを受け付ける
         if (Input.GetMouseButtonDown(0))
         {
             audioSource.PlayOneShot(SE1);
             Debug.Log("鳴ったー");
         }
+        
+
     }
 
 }
---
+++ boudu_soundmanader/Assets/Script/SoundBgm.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBgm : MonoBehaviour
+{
+    public AudioClip BGM;
+    public AudioSource audioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();//音を代入
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //右クリックを受け付ける
+        if (Input.GetMouseButtonDown(1))
+        {
+            audioSource.Stop();
+            Debug.Log("止まれー");
+            
+        }
+        //スペースで再生
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.Play();
+            Debug.Log("再生せよ！");
+        }
+
+    }
+}
