9597aea743b9dcc77d93c07268e625c58dc7b56a
kabuto <0cp1v1527251r6v@ezweb.ne.jp>
2020-06-26 18:24:31 +0900

    enumとlistは作れてるは思うけど呼び出し方がわからない

--- boudu_soundmanader/Assets/Script/SoundManader.cs
+++ boudu_soundmanader/Assets/Script/SoundManader.cs
@@ -1,34 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManader : MonoBehaviour
 {
-    public AudioSource audioSource;
+
     public enum Sounds
-    {
-        BGM1, //タイトル画面    
-        BGM2, //編集画面        
-        BGM3, //ゲーム画面      
-        BGM4, //リザルト画面    
-        SE1,  //ゲーム開始      
-        SE2,  //シャッフルする  
-        SE3,  //カードを加える  
-        SE4,  //カードを捨てる  
-        SE5,  //スキル発動      
-        SE6,  //ゲーム終了  
-        SE7   //決定
+     {
+         BGM1, //タイトル画面    
+         BGM2, //編集画面        
+         BGM3, //ゲーム画面      
+         BGM4, //リザルト画面   
 
-    };
+         SE1,  //ゲーム開始      
+         SE2,  //シャッフルする  
+         SE3,  //カードを加える  
+         SE4,  //カードを捨てる  
+         SE5,  //スキル発動      
+         SE6,  //ゲーム終了  
+         SE7   //決定
 
-    public List<Sounds> bgmList = new List<Sounds>();
-    public List<Sounds> seList = new List<Sounds>();
+     };
+
+    private List<Sounds> soundList = new List<Sounds>();
+
+    public AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
-    { }
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     { }
+
+    public void Taitle()
+    {
+        
+    }
+
 }
