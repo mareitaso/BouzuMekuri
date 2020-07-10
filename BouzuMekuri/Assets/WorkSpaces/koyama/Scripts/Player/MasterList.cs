534e7e0a9c66e8b64e3d0aed642d9cff11f20306
koyama <>
2020-07-10 14:41:44 +0900

    ランダム面の整理

--- BouzuMekuri/Assets/WorkSpaces/koyama/Scripts/Player/MasterList.cs
+++ BouzuMekuri/Assets/WorkSpaces/koyama/Scripts/Player/MasterList.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-//using System;
+using System;
 
 public class MasterList : MonoBehaviour
 {
@@ -25,15 +25,6 @@ public class MasterList : MonoBehaviour
     };
     private void Start()
     {
-        /*
-        foreach(var pmodel in playerModels)
-        {
-            var max = Random.Range(3, 6);
-            for(int i = 0; i < max; i++)
-            {
-                pmodel.Card.Add(Random.Range(5, 10));
-            }
-        }*/
         //リストの初期化
         if (list.Count > 0)
         {
@@ -50,116 +41,60 @@ public class MasterList : MonoBehaviour
     //手札のカード番号と枚数をランダムに並び替え
     public void Shuffle1()
     {
-        /*
-        //リストの初期化
-        if (list.Count > 0)
-        {
-            list.ForEach(cardList => cardList.Clear());
-            list.Clear();
-        }
-        
-        foreach (var pmodel in playerModels)
-        {
-            list.Add(pmodel.Card);
-        }
-        */
         //countListにcardListの数を格納
         var countList = list.Select(cardList => cardList.Count).ToList();
         //要素の格納リストの宣言
         List<int> totalList = new List<int>();
         //リストに要素を全部格納
         list.ForEach(cardList => totalList.AddRange(cardList));
-
-
-
-        /*
-        //1pの1枚目を表示
-        UnityEngine.Debug.Log(list[0][0]);
-        //int count = playerlist.Count;
-
-        for (int s = 0; s < playerlist.Count; s++)
-        {
-            list = list.OrderBy(a => Guid.NewGuid()).ToList();
-            UnityEngine.Debug.LogFormat("#{0}P:{1}枚",s+1);
-        }
-        return;
-        
-        
-
-        /*
-        for (int i = 0; i < _1.card.Count; i++)
-        {
-            list[0].Add(_1.card[i]);
-        }
-        /*
-        list[0] = _1.card;
-        List<int> e = new List<int>();
-        for(int i = 0; i < _1.card.count; i++)
-        {
-            e.Add(i);
-        }
-        list[1] = _2.card;
-        list[2] = _3.card;
-        list[3] = _4.card;
-        for(int i = 0; i <4; i++)
-        {
-            UnityEngine.Debug.LogFormat("#{0}P:{1}", i + 1, list[i]);
-        }*/
-
-        /*
-        //p1の枚数を追加
-        var list1 = new List<int>();
-        list1.Add(_1.card.Count);
-        //p2の枚数を追加
-        var list2 = new List<int>(list1);
-        list2.Add(_2.card.Count);
-        //p3の枚数を追加
-        var list3 = new List<int>(list2);
-        list3.Add(_3.card.Count);
-        //p4の枚数を追加
-        var list4 = new List<int>(list3);
-        list4.Add(_4.card.Count);
-
-         //現在枚数表示
-        for (int i = 0; i < list4.Count; i++)
-        {
-            UnityEngine.Debug.LogFormat("#{0}P:{1}枚",i+1,list4[i]);
-        }
-        //人数をカウント
-        int count = list4.Count;
-
-        for(int s = 0; s < count; s++)
-        {
-            //ランダムで値を結果リストに入れる
-            var list5 = list4[UnityEngine.Random.Range(0, list4.Count)];
-            //上記の値を全体リストから消去
-            list4.Remove(list5);
-            UnityEngine.Debug.LogFormat("#{0}P:{1}枚", s + 1, list5);
-            /*
-            _1.card = list5;
-            _2.card = list5;
-            _3.card = list5;
-            _4.card = list5;
-            UnityEngine.Debug.Log(_1.card);
-            UnityEngine.Debug.Log(_2.card);
-            UnityEngine.Debug.Log(_3.card);
-            UnityEngine.Debug.Log(_4.card);
-       
-        }
-        //_1.card = list5[0];
-        //_2.card = list5[1];
-        //_3.card = list5[2];
-        //_4.card = list5[3];
-        //UnityEngine.Debug.Log(_1.card);
-        //UnityEngine.Debug.Log(_2.card);
-        //UnityEngine.Debug.Log(_3.card);
-        //UnityEngine.Debug.Log(_4.card);
-        //*/
     }
-    //未完成
+    //ランダムに振り分け
     public void Shuffle2()
     {
+        //要素の格納リストの宣言
+        List<int> totalList = new List<int>();
+        //リストに要素を全部格納
+        list.ForEach(cardList => totalList.AddRange(cardList));
+        //シャッフル用の乱数
+        System.Random rnd = new System.Random();
+        //長さをコピー
+        int n = totalList.Count;
+        //シャッフル
+        while(n > 1)
+        {
+            n--;
+            int k = rnd.Next(n + 1);
+            int tmp = totalList[k];
+            totalList[k] = totalList[n];
+            totalList[n] = tmp;
+        }
+        //1Pから3P分のランダム判定
+        for(int i = 0; i <3; i++)
+        {
+            //範囲を決める
+            int index = UnityEngine.Random.Range(0,totalList.Count);
+            //中身を空にする
+            list[i].Clear();
+            //範囲内の値を移動させるまでループ
+            while (index>0) 
+            {
+                //index番目の値をコピーする
+                int r = totalList[index];
+                //index番目を中身に入れる
+                list[i].Add(r);
+                //index番目を除外する
+                totalList.RemoveAt(index);
+                index -= 1;
+            }
+        }
+        //余りを全部4Pへ
+        list[3].Clear();
+        for(int z = 0; z < totalList.Count; z++)
+        {
+            int r = totalList[0];
+            list[3].Add(r);
+            totalList.RemoveAt(0);
+        }
 
-        //int[] list1 = new int[]
     }
 }
