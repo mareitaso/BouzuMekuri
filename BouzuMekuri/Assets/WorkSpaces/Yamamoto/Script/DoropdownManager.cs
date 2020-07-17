3ca6bba6dae85460ace698aeaac2d8c650a777d6
kabuto <0cp1v1527251r6v@ezweb.ne.jp>
2020-07-17 17:20:54 +0900

    ルール編集画面

---
+++ BouzuMekuri/Assets/WorkSpaces/Yamamoto/Script/DoropdownManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoropdownManager : MonoBehaviour
+{
+    [SerializeField]
+    private Dropdown dropdown;
+
+    [SerializeField]
+    private GameObject image;//imageに格納する変数
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dropdown.value == 1)
+        {
+            image.GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (dropdown.value == 2)
+        {
+            image.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (dropdown.value == 3)
+        {
+            image.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+    }
+}
