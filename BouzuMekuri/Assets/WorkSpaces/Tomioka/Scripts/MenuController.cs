853af8ee218721bb05a6e00146fb76588b0b87f4
Tomioka <17c1020019tt@edu.tech.ac.jp>
2020-07-24 16:07:34 +0900

    メニュー作り

---
+++ BouzuMekuri/Assets/WorkSpaces/Tomioka/Scripts/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject MenuPanel;
+
+
+
+
+
+    //メニューをオンにする
+    public void MenuOn()
+    {
+        MenuPanel.SetActive(true);
+    }
+
+
+    //メニューをオフにする
+    public void MenuOff()
+    {
+        MenuPanel.SetActive(false);
+    }
+}
