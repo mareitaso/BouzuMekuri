93da3c590230e46be2529c42c72907d2f6dc00b4
kabuto <0cp1v1527251r6v@ezweb.ne.jp>
2020-07-24 15:35:29 +0900

    SetActive実装

---
+++ BouzuMekuri/Assets/WorkSpaces/Yamamoto/Script/Inputtext.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inputtext : MonoBehaviour
+{
+    //オブジェクトと結びつける
+    public InputField inputField;
+    public Text text;
+
+    void Start()
+    {
+        //Componentを扱えるようにする
+        inputField = inputField.GetComponent<InputField>();
+        text = text.GetComponent<Text>();
+
+    }
+
+    public void InputText()
+    {
+        //テキストにinputFieldの内容を反映
+        text.text = inputField.text;
+
+    }
+}
