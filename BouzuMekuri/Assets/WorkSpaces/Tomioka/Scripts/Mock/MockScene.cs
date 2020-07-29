d505973e08e70084ff8ea93de98b305da6f43528
Tomioka <17c1020019tt@edu.tech.ac.jp>
2020-07-30 06:15:01 +0900

    素材入れた

--- BouzuMekuri/Assets/WorkSpaces/Tomioka/Scripts/Mock/MockScene.cs
+++ BouzuMekuri/Assets/WorkSpaces/Tomioka/Scripts/Mock/MockScene.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class MockScene : MonoBehaviour
 {
+    [SerializeField]
+    private Image cloud1, cloud2;
+
+    [SerializeField]
+    private GameObject cloudPlace1, cloudPlace2;
+
     private void Start()
     {
         SoundManager.instance.BgmApply(Bgm.Title);
     }
     public void SceneLoad()
     {
-        SceneManager.LoadScene("ru-ruhebbsyuu");
-        //SoundManager.instance.FadeOutBgm(0.5f);
+        SoundManager.instance.FadeOutBgm(1f);
+
+        cloud1.transform.DOMove(cloudPlace1.transform.position, 1.5f);
+        cloud1.transform.DOMove(cloudPlace1.transform.position, 1.5f).OnComplete(() =>
+        {
+
+            SceneManager.LoadScene("ru-ruhebbsyuu");
+        });
     }
 }
