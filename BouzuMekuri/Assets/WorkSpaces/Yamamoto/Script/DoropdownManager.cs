f94dcbfedbce45f9832c631e858d41184e6f5e49
kabuto <0cp1v1527251r6v@ezweb.ne.jp>
2020-08-17 11:22:17 +0900

    スキル1,2を表示させる dropdownの値を0にセットし直す

--- BouzuMekuri/Assets/WorkSpaces/Yamamoto/Script/DoropdownManager.cs
+++ BouzuMekuri/Assets/WorkSpaces/Yamamoto/Script/DoropdownManager.cs
@@ -6,25 +6,101 @@ using UnityEngine.UI;
 public class DoropdownManager : MonoBehaviour
 {
     [SerializeField]
-    private Dropdown dropdown;
-
+    private GameObject Panel1;
     [SerializeField]
-    private GameObject image;//imageに格納する変数
+    private GameObject Panel2;
+    [SerializeField]
+    private GameObject Panel3;
+    [SerializeField]
+    private GameObject Panel4;
+    [SerializeField]
+    private Dropdown dropdown1;
+    [SerializeField]
+    private Dropdown dropdown2;
+    [SerializeField]
+    private GameObject text1;
+    [SerializeField]
+    private GameObject text2;
 
-    // Update is called once per frame
-    void Update()
+    // Use this for initialization
+    public void Drop1()
     {
-        if (dropdown.value == 1)
+        if (dropdown1.value == 0)
         {
-            image.GetComponent<Renderer>().material.color = Color.red;
+            Panel1.SetActive(false);
+            Panel2.SetActive(false);
+            text1.SetActive(false);
+            Debug.Log("なし");
+            SetValue(0);
         }
-        if (dropdown.value == 2)
+        else if (dropdown1.value == 1)
         {
-            image.GetComponent<Renderer>().material.color = Color.blue;
+            Panel1.SetActive(true);
+            text1.SetActive(true);
+            Debug.Log("天皇");
+            SetValue(1);
         }
-        if (dropdown.value == 3)
+        else if (dropdown1.value == 2)
         {
-            image.GetComponent<Renderer>().material.color = Color.yellow;
+            Panel2.SetActive(true);
+            text1.SetActive(true);
+            Debug.Log("段付き");
+            SetValue(2);
         }
+        else
+        {
+            Panel1.SetActive(false);
+            Panel2.SetActive(false);
+            text1.SetActive(false);
+            SetValue(0);
+        }
+        
+    }
+    public void SetValue(int value)
+    {
+        dropdown1.value = value;
+    }
+    public void Drop2()
+    { 
+        if (dropdown2.value == 0)
+        {
+            Panel3.SetActive(false);
+            Panel4.SetActive(false);
+            text2.SetActive(false);
+            Debug.Log("なし");
+            SetValue2(0);
+        }
+        else if (dropdown2.value == 1)
+        {
+            Panel3.SetActive(true);
+            text2.SetActive(true);
+            Debug.Log("武官");
+            SetValue2(1);
+        }
+        else if (dropdown2.value == 2)
+        {
+            Panel4.SetActive(true);
+            text2.SetActive(true);
+            Debug.Log("弓持ち");
+            SetValue2(2);
+        }
+        else
+        {
+            Panel3.SetActive(false);
+            Panel4.SetActive(false);
+            text2.SetActive(false);
+            SetValue2(0);
+        }
+        
+    }
+    public void SetValue2(int value2)
+    {
+        dropdown2.value = value2;
+    }
+    public void OnClick()
+    {
+        dropdown1.value = 0;
+        dropdown2.value = 0;
     }
 }
+//ボタンを押したら～に変える
