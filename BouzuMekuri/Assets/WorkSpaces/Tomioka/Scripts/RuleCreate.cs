f50a7b6f2bb44f03857967cc325c0a9f607d9ada
Tomioka <17c1020019tt@edu.tech.ac.jp>
2020-08-20 14:44:23 +0900

    じさく調整

---
+++ BouzuMekuri/Assets/WorkSpaces/Tomioka/Scripts/RuleCreate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuleCreate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonBuck()
+    {
+        SceneController.instance.LoadScene(SceneController.SceneName.Rule);
+    }
+}
