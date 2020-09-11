5b59411eec57237fef6830ad00851b236269516a
Tomioka <17c1020019tt@edu.tech.ac.jp>
2020-09-11 10:56:53 +0900

    メニューでタイトルに戻る際本当に戻りますか？を実装

--- BouzuMekuri/Assets/WorkSpaces/Tomioka/Scripts/MenuController.cs
+++ BouzuMekuri/Assets/WorkSpaces/Tomioka/Scripts/MenuController.cs
@@ -8,6 +8,8 @@ public class MenuController : MonoBehaviour
     [SerializeField]
     private GameObject MenuPanel;
 
+    [SerializeField]
+    private GameObject titlePanel;
 
     //メニューをオンにする
     public void MenuOn()
@@ -21,11 +23,25 @@ public class MenuController : MonoBehaviour
     {
         MenuPanel.SetActive(false);
     }
-    
+
+
+    /// <summary>
+    ///タイトルに戻るを押すと「本当に戻りますか」
+    ///と聞くパネルを出す
+    /// </summary>
+    public void TitleButton()
+    {
+        titlePanel.SetActive(true);
+    }
+
     public void LoadTitle()
     {
         SoundManager.instance.FadeOutBgm(1f);
         ReSetCommand.instance.ReSet();
     }
 
+    public void MenuBack()
+    {
+        titlePanel.SetActive(false);
+    }
 }
