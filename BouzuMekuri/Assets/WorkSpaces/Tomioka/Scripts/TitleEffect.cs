25d23b4874679674f9baa32db96d0ba1cddd19a2
Tomioka <17c1020019tt@edu.tech.ac.jp>
2020-09-23 17:11:33 +0900

    いろいろおわた

--- BouzuMekuri/Assets/WorkSpaces/Tomioka/Scripts/TitleEffect.cs
+++ BouzuMekuri/Assets/WorkSpaces/Tomioka/Scripts/TitleEffect.cs
@@ -14,7 +14,7 @@ public class TitleEffect : MonoBehaviour
 
     private void Start()
     {
-        SoundManager.instance.BgmApply(Bgm.Title);
+        Invoke("StartBGM", 1f);
     }
 
     private void Update()
@@ -23,6 +23,11 @@ public class TitleEffect : MonoBehaviour
         {
             SceneLoad();
         }
+    }
+
+    private void StartBGM()
+    {
+        SoundManager.instance.BgmApply(Bgm.Title);
     }
 
     public void SceneLoad()
