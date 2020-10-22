3db1f75e146ce4ae0df195488b0d982221f08251
kabuto <0cp1v1527251r6v@ezweb.ne.jp>
2020-10-22 14:02:09 +0900

    自作ルールを先に選んだら、選んだ選択肢に応じてルール１orルール２を押せなくなるに修正

--- BouzuMekuri/Assets/WorkSpaces/Yamamoto/Script/DoropdownManager.cs
+++ BouzuMekuri/Assets/WorkSpaces/Yamamoto/Script/DoropdownManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class DoropdownManager : MonoBehaviour
 {
@@ -19,27 +20,11 @@ public class DoropdownManager : MonoBehaviour
     private Dropdown dropdown2;//ルール２
     [SerializeField]
     private Dropdown dropdown3;
-    [SerializeField]
-    private Dropdown dropdown4;//ルール１天
-    [SerializeField]
-    private Dropdown dropdown5;//ルール１段
-    [SerializeField]
-    private Dropdown dropdown6;//ルール２武
-    [SerializeField]
-    private Dropdown dropdown7;//ルール２弓
 
     [SerializeField]
-    private GameObject Panel5;//ルール１
+    private UnityEngine.UI.Dropdown rule1;//ルール１
     [SerializeField]
-    private GameObject Panel6;//ルール１天
-    [SerializeField]
-    private GameObject Panel7;//ルール１段
-    [SerializeField]
-    private GameObject Panel8;//ルール２
-    [SerializeField]
-    private GameObject Panel9;//ルール２武
-    [SerializeField]
-    private GameObject Panel10;//ルール２弓
+    private UnityEngine.UI.Dropdown rule2;//ルール２
 
     [SerializeField]
     private GameObject text1;
@@ -47,7 +32,7 @@ public class DoropdownManager : MonoBehaviour
     private GameObject text2;
 
     [SerializeField]
-    private Dropdown dropdown8;//自作から参照
+    private Dropdown dropdown4;//自作から参照
 
     [SerializeField]
     private GameObject RulePanel;//ローカルシーン
@@ -56,55 +41,21 @@ public class DoropdownManager : MonoBehaviour
     // Use this for initialization
     public void Drop1()
     {
-        if (dropdown8.value == 0)//未選択
+        if (dropdown4.value == 0)//未選択
         {
-            Panel5.SetActive(true);
-            Panel6.SetActive(false);
-            Panel7.SetActive(false);
-            Panel8.SetActive(true);
-            Panel9.SetActive(false);
-            Panel10.SetActive(false);
-            
+            rule1.interactable = true;
+            rule2.interactable = true;
         }
-        else if (dropdown8.value == 1) //天なし
+        else if (dropdown4.value == 1 || dropdown4.value == 2)  //天段なし
         {
-            Panel5.SetActive(false);
-            Panel6.SetActive(false);
-            Panel7.SetActive(true);
-            Panel8.SetActive(true);
-            Panel9.SetActive(false);
-            Panel10.SetActive(false);
-            
+            rule1.interactable = false;
+            rule2.interactable = true;
         }
-        else if (dropdown8.value == 2) //段なし
+        
+        else if (dropdown4.value == 3 || dropdown4.value == 4) 　//武弓なし
         {
-            Panel5.SetActive(false);
-            Panel6.SetActive(true);
-            Panel7.SetActive(false);
-            Panel8.SetActive(true);
-            Panel9.SetActive(false);
-            Panel10.SetActive(false);
-            
-        }
-        else if (dropdown8.value == 3)　//武なし
-        {
-            Panel5.SetActive(true);
-            Panel6.SetActive(false);
-            Panel7.SetActive(false);
-            Panel8.SetActive(false);
-            Panel9.SetActive(false);
-            Panel10.SetActive(true);
-            
-        }
-        else if (dropdown8.value == 4)　//弓なし
-        {
-            Panel5.SetActive(true);
-            Panel6.SetActive(false);
-            Panel7.SetActive(false);
-            Panel8.SetActive(false);
-            Panel9.SetActive(true);
-            Panel10.SetActive(false);
-            
+            rule1.interactable = true;
+            rule2.interactable = false;
         }
     }
     public void Drop2()
@@ -187,131 +138,6 @@ public class DoropdownManager : MonoBehaviour
     {
         dropdown2.value = value2;
     }
-    public void Drop4()
-    {
-        if (dropdown4.value == 0)
-        {
-            Panel1.SetActive(false);
-            Panel2.SetActive(false);
-            text1.SetActive(false);
-            RuleManager.instance.PlayerList[RuleCreate.instance.PlayerNumber].RuleList[0].RuleEfect[0] = 0;
-            Debug.Log("なし");
-            SetValue4(0);
-        }
-        else if (dropdown4.value == 1)
-        {
-            Panel1.SetActive(true);
-            Panel2.SetActive(false);
-            text1.SetActive(true);
-            Debug.Log("天皇");
-            SetValue4(1);
-        }
-        else
-        {
-            Panel1.SetActive(false);
-            Panel2.SetActive(false);
-            text1.SetActive(false);
-            SetValue4(0);
-        }
-
-    }
-    public void SetValue4(int value4)
-    {
-        dropdown4.value = value4;
-    }
-    public void Drop5()
-    {
-        if (dropdown5.value == 0)
-        {
-            Panel1.SetActive(false);
-            Panel2.SetActive(false);
-            text1.SetActive(false);
-            RuleManager.instance.PlayerList[RuleCreate.instance.PlayerNumber].RuleList[0].RuleEfect[0] = 0;
-            Debug.Log("なし");
-            SetValue5(0);
-        }
-        else if (dropdown5.value == 1)
-        {
-            Panel1.SetActive(false);
-            Panel2.SetActive(true);
-            text1.SetActive(true);
-            Debug.Log("段付き");
-            SetValue5(1);
-        }
-        else
-        {
-            Panel1.SetActive(false);
-            Panel2.SetActive(false);
-            text1.SetActive(false);
-            SetValue5(0);
-        }
-    }
-    public void SetValue5(int value5)
-    {
-        dropdown5.value = value5;
-    }
-    public void Drop6()
-    {
-        if (dropdown6.value == 0)
-        {
-            Panel3.SetActive(false);
-            Panel4.SetActive(false);
-            text2.SetActive(false);
-            RuleManager.instance.PlayerList[RuleCreate.instance.PlayerNumber].RuleList[0].RuleEfect[0] = 0;
-            Debug.Log("なし");
-            SetValue6(0);
-        }
-        else if (dropdown6.value == 1)
-        {
-            Panel3.SetActive(true);
-            Panel4.SetActive(false);
-            text2.SetActive(true);
-            Debug.Log("武官");
-            SetValue6(1);
-        }
-        else
-        {
-            Panel3.SetActive(false);
-            Panel4.SetActive(false);
-            text2.SetActive(false);
-            SetValue6(0);
-        }
-    }
-    public void SetValue6(int value6)
-    {
-        dropdown6.value = value6;
-    }
-    public void Drop7()
-    {
-        if (dropdown7.value == 0)
-        {
-            Panel3.SetActive(false);
-            Panel4.SetActive(false);
-            text2.SetActive(false);
-            RuleManager.instance.PlayerList[RuleCreate.instance.PlayerNumber].RuleList[0].RuleEfect[0] = 0;
-            Debug.Log("なし");
-            SetValue7(0);
-        }
-        else if (dropdown7.value == 1)
-        {
-            Panel3.SetActive(false);
-            Panel4.SetActive(true);
-            text2.SetActive(true);
-            Debug.Log("弓持ち");
-            SetValue7(1);
-        }
-        else
-        {
-            Panel3.SetActive(false);
-            Panel4.SetActive(false);
-            text2.SetActive(false);
-            SetValue7(0);
-        }
-    }
-    public void SetValue7(int value7)
-    {
-        dropdown7.value = value7;
-    }
      
     public void OnClick()
     {
@@ -319,10 +145,6 @@ public class DoropdownManager : MonoBehaviour
         dropdown2.value = 0;
         dropdown3.value = 0;
         dropdown4.value = 0;
-        dropdown5.value = 0;
-        dropdown6.value = 0;
-        dropdown7.value = 0;
-        dropdown8.value = 0;
     }
     public void OnClickS1()//ローカルから自作
     {
